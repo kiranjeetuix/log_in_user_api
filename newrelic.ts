4881f03394e98e8cdab7c22680d85461dd4f7671
kiranthakurr <heykiranthakur@gmail.com>
2024-06-23 17:21:36 +0530

    abc

---
+++ newrelic.ts
@@ -0,0 +1,24 @@
+'use strict';
+import './.env';
+exports.config = {
+  app_name: ['login_repo'],
+  license_key: 'e76e959da52608fb3b5a00f7a4cfbff2FFFFNRAL',
+  logging: {
+    level: 'info',
+  },
+  allow_all_headers: true,
+  attributes: {
+    exclude: [
+      'request.headers.cookie',
+      'request.headers.authorization',
+      'request.headers.proxyAuthorization',
+      'request.headers.setCookie*',
+      'request.headers.x*',
+      'response.headers.cookie',
+      'response.headers.authorization',
+      'response.headers.proxyAuthorization',
+      'response.headers.setCookie*',
+      'response.headers.x*',
+    ],
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
+import './instrument.js';
 import { AppModule } from './app.module';
+import * as Sentry from '@sentry/node';
+import {
+  BaseExceptionFilter,
+  HttpAdapterHost,
+  NestFactory,
+} from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  Sentry.setupNestErrorHandler(app, new BaseExceptionFilter(httpAdapter));
   await app.listen(3000);
 }
 bootstrap();
